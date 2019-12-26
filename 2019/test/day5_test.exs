0bcb497b4894a87b1244e29011037f1865d47297
Solomon Hawk <solomon.hawk@viget.com>
2019-12-26 02:11:10 -0500

    mix format

--- 2019/test/day5_test.exs
+++ 2019/test/day5_test.exs
@@ -7,11 +7,11 @@ defmodule Day5Test do
   alias Intcode.ExecutionContext.Adapters.Memory, as: MemoryAdapter
 
   @longer_part2_example """
-         3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
-         1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
-         999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
-       """
-       |> Intcode.Program.parse()
+                          3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+                          1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+                          999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+                        """
+                        |> Intcode.Program.parse()
 
   describe "Intcode.Processor with IO Adapter" do
     test "3,0,4,0,99 gets an input from :stdin and outputs it to :stdout" do
